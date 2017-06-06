b9f7219fe1087c68182c49341202d63cf88e22e5
Luis Alberto <fernandezc.alberto@gmail.com>
2017-06-06 20:22:54 +0200

    modal window fully functional and code more modularized

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,15 +1,15 @@
 console.log('main.js was loaded....')
 
-$('button.modal-btn').on('click', function (e) {
-  e.preventDefault()
-  const url = 'recipes'
-  const $id = $(this).siblings('[name="id"]').val()
+// $('button.modal-btn').on('click', function (e) {
+//   e.preventDefault()
+//   const url = 'recipes'
+//   const $id = $(this).siblings('[name="id"]').val()
 
-  console.log($id)
+//   console.log($id)
 
-  $.ajax({
-    url,
-    data: $id,
-    method: 'POST'
-  })
-})
+//   $.ajax({
+//     url,
+//     data: $id,
+//     method: 'POST'
+//   })
+// })
--- server/routes/recipes/handlers/getAll.js
+++ server/routes/recipes/handlers/getAll.js
@@ -1,13 +1,11 @@
 const Recipe = require ('../../../models/Recipe')
 
 function getAll (req, res) {
-  const {$id} = req.body
   console.log('requesting all recipes...')
   Recipe.find()
     .then(data => {
       console.log('Showing all recipes')
-      console.log(data)
-      res.render('recipes', {data, $id})
+      res.render('recipes', {data})
     })
 }
 
