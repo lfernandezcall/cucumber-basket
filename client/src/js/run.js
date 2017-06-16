fe9f13d904e7bc4f6d70afa5e214a6aa1ae677ed
Luis Alberto <fernandezc.alberto@gmail.com>
2017-06-16 13:00:38 +0200

    added ingredients to modal and more...

--- client/src/js/run.js
+++ client/src/js/run.js
@@ -1,10 +1,10 @@
-var fs = require('fs');
-var htmlNavbar = fs.readFileSync(__dirname + '/templates/navbar.html', 'utf8');
-var htmlModal = fs.readFileSync(__dirname + '/templates/footer.html', 'utf8');
+var fs = require('fs')
+var htmlNavbar = fs.readFileSync(__dirname + '/templates/navbar.html', 'utf8')
+var htmlFooter = fs.readFileSync(__dirname + '/templates/footer.html', 'utf8')
 
-function runAtBegin($templateCache) {
-  $templateCache.put('navbar.html', htmlNavbar);
-  $templateCache.put('footer.html', htmlModal);
+function runAtBegin ($templateCache) {
+  $templateCache.put('navbar.html', htmlNavbar)
+  $templateCache.put('footer.html', htmlFooter)
 }
 
 module.exports = runAtBegin
