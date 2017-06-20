ee17b8dad6d5b8a4c69b01d13e6d2d8f1991e807
Luis Alberto Fernández Calleja <Efea@iMac-de-Luis.local>
2017-06-20 17:13:33 +0200

    fixed size thumbnails

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,12 +1,10 @@
 const angular = require('angular')
 
-const ngAnimate = require('angular-animate')
-const toastr = require('angular-toastr')
 const runAtBegin = require('./run')
 
 const routesApp = require('./routes')
 const DataService = require('./services/DataService')
 
-angular.module('adminCollection', [ routesApp, ngAnimate, toastr ])
+angular.module('adminCollection', [ routesApp ])
   .factory('DataService', DataService)
   .run(runAtBegin)
