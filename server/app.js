bf4127cf7c47aed306ea1ce17c540277ce60e311
Luis Alberto <fernandezc.alberto@gmail.com>
2017-07-26 15:50:53 +0200

    refactor routes express and local upload working

--- server/app.js
+++ server/app.js
@@ -2,20 +2,11 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const multer = require('multer')
 
 if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
 
 const dbUrl = process.env.DB_URL
 const PORT = process.env.PORT
-const PATH = process.env.PATH
-
-console.log( `This is the path of the .env archive: ${PATH}` )
-
-const upload = multer({
-	dest: PATH
-})
-
 
 mongoose.promise = Promise
 mongoose.connect(dbUrl)
@@ -24,26 +15,20 @@ const app = express()
 
 app.use(express.static('client/public'))
 app.use(express.static('server/data'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 
+const routerUpload = require('./routes/upload')
 const routerHome = require('./routes/home')
 const routerRecipes = require('./routes/recipes')
 const routerApiRecipe = require('./routes/api/recipe')
 const routerApiRecipes = require('./routes/api/recipes')
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-
-app.post('/upload', upload.single('file'), (req, res) => {
-	res.send(JSON.stringify(req.file))
-})
-
-app.get('/', (eq, res) => {
-  res.redirect('/home')
-})
+app.use('/upload', routerUpload)
+app.use('/', routerHome)
 app.use('/home', routerHome)
 app.use('/recipes', routerRecipes)
 app.use('/api/recipe', routerApiRecipe)
--- server/routes/upload/index.js
+++ server/routes/upload/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('multer')
 
-const uploadImage = require('./handlers/uploadImage')
+if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
 
-router.post('/upload', uploadImage)
+const PATH = process.env.ROUTEUPLOADS
+const upload = multer({
+  dest: PATH
+})
+
+router.post('/', upload.single('file'), (req, res) => {
+  res.send(JSON.stringify(req.file))
+})
 
 module.exports = router
